b1eeccdf21ec14888ba8f2691c5841097d339163
Stella Zarei <zareistella@gmail.com>
2024-09-26 15:38:31 -0700

    Adds rgb to hsv and hsv to rgb implementations

--- src/hsv_to_rgb.cpp
+++ src/hsv_to_rgb.cpp
@@ -1,17 +1,56 @@
 #include "hsv_to_rgb.h"
+#include <cmath>
 
+/**
+ * Convert the color from HSV to RGB
+ * 
+ */
 void hsv_to_rgb(
-  const double h,
-  const double s,
-  const double v,
-  double & r,
-  double & g,
-  double & b)
+    const double h,
+    const double s,
+    const double v,
+    double & r,
+    double & g,
+    double & b)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  r = 0;
-  g = 0;
-  b = 0;
-  ////////////////////////////////////////////////////////////////////////////
+    int category = h / 60;
+    double c = s * v;
+    double x = c * (1 - std::abs(std::fmod(h / 60, 2) - 1));
+    double m = v - c;
+
+    switch (category)
+    {   
+        case 0:
+            r = c;
+            g = x;
+            b = 0;
+            break;
+        case 1: 
+            r = x;
+            g = c;
+            b = 0;
+            break;
+        case 2:
+            r = 0;
+            g = c;
+            b = x;
+            break;
+        case 3:
+            r = 0;
+            g = x;
+            b = c;
+            break;
+        case 4:
+            r = x;
+            g = 0;
+            b = c;
+            break;
+        case 5:
+            r = c;
+            g = 0;
+            b = x;
+            break;
+    }
+
+    r += m; g += m; b += m;
 }
--- src/rgb_to_hsv.cpp
+++ src/rgb_to_hsv.cpp
@@ -1,17 +1,55 @@
 #include "rgb_to_hsv.h"
+#include <algorithm>
 
+/**
+ * Convert RGB to HSV
+ * 
+ * Convert a color represented by red, green and blue intensities to its equivalent hue, saturation and value.
+ * 
+ * Logic:
+ *      1. Normalize the RGB values by dividing by 255
+ *      2. Find the min and max RGB values 
+ *      3. Calculate the delta = abs(max, min)
+ *      4. Calculate H
+ *      5. Calculate S = delta / max
+ *      6. Calcualte V = max
+ */
 void rgb_to_hsv(
-  const double r,
-  const double g,
-  const double b,
-  double & h,
-  double & s,
-  double & v)
+    const double r,
+    const double g,
+    const double b,
+    double & h,
+    double & s,
+    double & v)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  h = 0;
-  s = 0;
-  v = 0;
-  ////////////////////////////////////////////////////////////////////////////
+
+    // Step 1
+    double red = r / 255;
+    double green = g / 255;
+    double blue = b / 255;
+
+    // Step 2
+    double rgbMax = std::max({red, green, blue});
+    double rgbMin = std::min({red, green,blue});
+
+    // Step 3
+    double delta = std::abs(rgbMax - rgbMin);
+
+    // Step 4
+    if (rgbMax == red)
+    {
+        h = 60 * ((green - blue) / delta);
+    }
+    else if (rgbMax == green)
+    {
+        h = 60 * ((blue - red) / delta) + 120;
+    }
+    else
+    {
+        h = 60 * ((red - green) / delta) + 240;
+    }
+
+    // Steps 5 & 6
+    s = delta / rgbMax;
+    v = rgbMax;
 }
